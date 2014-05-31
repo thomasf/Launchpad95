bf35ae39e009dcd9bea8b96b8f3d9945e07fb061
Thomas Frössman <thomasf@jossystem.se>
2014-05-31 08:12:54 +0200

    just ran autopep8 on everything

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from Launchpad import Launchpad
 
+
 def create_instance(c_instance):
     """ Creates and returns the Launchpad script """
     return Launchpad(c_instance)
@@ -7,6 +8,7 @@ def create_instance(c_instance):
 
 from _Framework.Capabilities import *
 
+
 def get_capabilities():
     return {CONTROLLER_ID_KEY: controller_id(vendor_id=4661, product_ids=[14], model_name='Launchpad'),
-     PORTS_KEY: [inport(props=[NOTES_CC, REMOTE, SCRIPT]), outport(props=[NOTES_CC, REMOTE, SCRIPT])]}
+            PORTS_KEY: [inport(props=[NOTES_CC, REMOTE, SCRIPT]), outport(props=[NOTES_CC, REMOTE, SCRIPT])]}
